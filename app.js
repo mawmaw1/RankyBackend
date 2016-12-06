c90dd6d8d4e4ad4b7995654adc420a1758258233
mawmaw1 <magnus@Magnuss-MacBook-Air.local>
2016-12-06 13:10:34 +0100

    changes to path

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/ranky', express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/api', project);
-app.use('/api',players);
-app.use('/api', matches);
+app.use('/ranky/api', project);
+app.use('/ranky/api',players);
+app.use('/ranky/api', matches);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
